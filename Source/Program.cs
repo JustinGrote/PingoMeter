d7e211281d137a4d81f4473a624dc48902d636b1
Justin Grote <JustinGrote@users.noreply.github.com>
2026-02-08 23:43:33 -0800

    Bump Version: 1.0.0-alpha1

--- Source/Program.cs
+++ Source/Program.cs
@@ -5,7 +5,7 @@ namespace PingoMeter
     internal static class Program
     {
         /// <summary> x.x.x program version string. </summary>
-        public const string VERSION = "0.9.9";
+        public const string VERSION = "1.0.0-alpha1";
 
         [STAThread]
         public static void Main(string[] args)

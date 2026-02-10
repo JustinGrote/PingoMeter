5faa7c91e2b5d1f3697e59145315e366036c97c2
Justin Grote <JustinGrote@users.noreply.github.com>
2026-02-10 15:26:53 -0800

    Add winget and chocolatey deployment support

---
+++ Installer/Chocolatey/PingoMeter/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$VERSION = '__VERSION__'
+$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://github.com/JustinGrote/PingoMeter/releases/download/v$VERSION/PingoMeter-$VERSION-x86.msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi' #only one of these: exe, msi, msu
+  url           = $url
+  softwareName  = 'PingoMeter*'
+  checksum      = '__CHECKSUM__'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
